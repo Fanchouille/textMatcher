3c0120744079153cd6bc303242936e6c5713592c
Fanchouille <francois.valadier@openvalue.fr>
2017-09-19 13:25:37 +0200

    Cleaning of code

--- textMatcher.py
+++ textMatcher.py
@@ -7,6 +7,7 @@ from scipy.sparse import csr_matrix
 
 
 class TextMatcher:
+    # See https://labs.yodas.com/large-scale-matrix-multiplication-with-pyspark-or-how-to-match-two-large-datasets-of-company-1be4b1b2871e
     def __init__(self, input_dfs, text_cols, id_cols, stop_words=None):
         """
         Initialize :
@@ -22,9 +23,9 @@ class TextMatcher:
         return
 
     def get_vocabulary(self):
-
         """
         Concatenate all texts and create vocabulary from unique
+        :return:
         """
         vect = CountVectorizer(self.stop_words)
         all_texts = np.unique(np.stack((self.input_dfs[0].loc[:, self.text_cols[0]].values,
@@ -34,26 +35,22 @@ class TextMatcher:
         return
 
     def get_tfidf_matrices(self):
+        """
+        Creates the 2 TFIdf matrices
+        :return:
+        """
         self.get_vocabulary()
         tfidf_vect = TfidfVectorizer(stop_words=self.stop_words, vocabulary=self.vocabulary)
-        XTf1 = self.input_dfs[0].loc[:, self.text_cols[0]].values
-        XTf2 = self.input_dfs[1].loc[:, self.text_cols[1]].values
+        XTf1 = tfidf_vect.fit_transform(self.input_dfs[0].loc[:, self.text_cols[0]].values)
+        XTf2 = tfidf_vect.fit_transform(self.input_dfs[1].loc[:, self.text_cols[1]].values)
+        self.tf_matrices = (XTf1, XTf2)
         return
 
-    def find_matches_in_submatrix(self, sources, targets, inputs_start_index, threshold=.9):
-        cosimilarities = cosine_similarity(sources, targets)
-        for i, cosimilarity in enumerate(cosimilarities):
-            cosimilarity = cosimilarity.flatten()
-            # Find the best match by using argsort()[-1]
-            target_index = cosimilarity.argsort()[-1]
-            source_index = inputs_start_index + i
-            similarity = cosimilarity[target_index]
-            if (cosimilarity[target_index] >= threshold) & (source_index != target_index):
-                yield (source_index, target_index, similarity)
-
-    def parallelize_matrix(self, scipy_mat, rows_per_chunk=100):
+    def parallelize_matrix(self, scipy_mat, rows_per_chunk):
         """
-        Creates chunk of matrix from a big one : with index / sparse sub matrix / shape
+        :param scipy_mat: Scipy Matrix
+        :param rows_per_chunk: # of rows per chunk
+        :return: Creates chunks of matrix from a big one : with index / sparse sub matrix / shape
         """
         [rows, cols] = scipy_mat.shape
         i = 0
@@ -69,22 +66,66 @@ class TextMatcher:
             i += current_chunk_size
         return submatrices
 
-    def get_results(self):
+    def find_matches_in_submatrix(self, sources, targets, inputs_start_index, threshold):
+        """
+        :param sources:
+        :param targets:
+        :param inputs_start_index:
+        :param threshold:
+        :return:
+        """
+        cosimilarities = cosine_similarity(sources, targets)
+        for i, cosimilarity in enumerate(cosimilarities):
+            cosimilarity = cosimilarity.flatten()
+            # Find the best match by using argsort()[-1]
+            target_index = cosimilarity.argsort()[-1]
+            source_index = inputs_start_index + i
+            similarity = cosimilarity[target_index]
+            if (cosimilarity[target_index] >= threshold) & (source_index != target_index):
+                yield (source_index, target_index, similarity)
 
-    all_matrices = parallelize_matrix(X_tf)
+    def get_results(self, rows_per_chunk=100, threshold=0.8):
+        """
 
-    results_generator = (find_matches_in_submatrix(csr_matrix(matrix[1],
-                                                              shape=matrix[2]),
-                                                   X_tf,
-                                                   matrix[0],
-                                                   threshold=.9) for matrix in all_matrices)
+        :param rows_per_chunk: # of rows per chunk
+        :param threshold: 1 for a perfect match
+        :return: a DF with 1st Id / 1st Text / 2nd Id / 2nd Text / Cosine Sim
+        """
+        self.get_tfidf_matrices()
+        all_matrices = self.parallelize_matrix(self.XTf1)
 
-    nearest_frn = []
-    for res in results_generator:
-        for x in res:
-            nearest_frn.append((filetab.loc[:, 'CODE_FOUR'].iloc[x[0]],
-                                filetab.loc[:, 'CODE_FOUR'].iloc[x[1]],
-                                filetab.loc[:, 'L_FOUR_CLEANED'].iloc[x[0]],
-                                filetab.loc[:, 'L_FOUR_CLEANED'].iloc[x[1]]))
+        results_generator = (self.find_matches_in_submatrix(csr_matrix(matrix[1], shape=matrix[2]),
+                                                            self.XTf2,
+                                                            matrix[0],
+                                                            threshold=.9)
+                             for matrix in all_matrices)
 
-            pd.DataFrame(nearest_frn, columns=['CODE_ORIG', 'CODE_LINKED', 'L_FOUR_ORIG', 'L_FOUR_LINKED'])
+        nearest_frn = []
+        for res in results_generator:
+            for x in res:
+                nearest_frn.append((self.input_dfs[0].loc[:, self.id_cols[0]].iloc[x[0]],
+                                    self.input_dfs[1].loc[:, self.id_cols[1]].iloc[x[1]],
+                                    self.input_dfs[0].loc[:, self.text_cols[0]].iloc[x[0]],
+                                    self.input_dfs[1].loc[:, self.text_cols[1]].iloc[x[1]],
+                                    x[2]
+                                    )
+                                   )
+
+        if self.id_cols[0] == self.id_cols[1]:
+            id_col_1 = self.id_cols[0] + '_1'
+            id_col_2 = self.id_cols[0] + '_2'
+        else:
+            id_col_1 = self.id_cols[0]
+            id_col_2 = self.id_cols[1]
+
+        if self.text_cols[0] == self.text_cols[1]:
+            text_col_1 = self.text_cols[0] + '_1'
+            text_col_2 = self.text_cols[0] + '_2'
+        else:
+            text_col_1 = self.text_cols[0]
+            text_col_2 = self.text_cols[1]
+
+        lCols = [id_col_1, text_col_1, id_col_2, text_col_2, 'COSINE_SIM']
+        oDf = pd.DataFrame(nearest_frn, columns=lCols)
+
+        return oDf
